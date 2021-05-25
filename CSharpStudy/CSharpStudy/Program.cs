c46b4fb8e58b57a20845456a0558e03a25c87990
HongEunbeen <37995685+HongEunbeen@users.noreply.github.com>
2021-05-26 00:13:35 +0900

    feat : 4. C# 변수와 선언

--- CSharpStudy/CSharpStudy/Program.cs
+++ CSharpStudy/CSharpStudy/Program.cs
@@ -6,8 +6,35 @@ namespace CSharpStudy
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("안녕 C#");
-            Console.Write("안녕 하세요");
+            short shotNum = 0;
+
+            int intNum = 0;
+
+            double doubleNum = 0;
+
+            float floatNum = 0;
+
+            string stringTest = "";
+
+            char charText = 'a';
+
+            bool boolVal = true;
+
+            Int16 dotNetShort = 0;
+
+            Int32 dotNetInt = 0;
+
+            Int64 dotNetDouble = 0;
+
+            String dotNetString = "";
+
+            var dynamicType = 0;
+
+            var num1 = 1;
+
+            var num2 = 2;
+
+            Console.WriteLine(num1 + num2);
         }
     }
 }
