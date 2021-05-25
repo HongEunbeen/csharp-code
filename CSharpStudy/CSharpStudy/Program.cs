541bc47c3e6b0ef56866dbad70fbdc3621b5d5c8
HongEunbeen <37995685+HongEunbeen@users.noreply.github.com>
2021-05-25 23:59:14 +0900

    feat : 3. C# Console 간단하게 살펴보기

---
+++ CSharpStudy/CSharpStudy/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CSharpStudy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("안녕 C#");
+            Console.Write("안녕 하세요");
+        }
+    }
+}
