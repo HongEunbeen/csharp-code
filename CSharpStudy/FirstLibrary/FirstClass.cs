d6a1c598eb2ac868a384705696ab163002df23bf
HongEunbeen <37995685+HongEunbeen@users.noreply.github.com>
2021-05-27 23:42:34 +0900

    feat : 2. ASP.NET MVC Core 프로젝트 만들기

---
+++ CSharpStudy/FirstLibrary/FirstClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace FirstLibrary
+{
+    public class FirstClass
+    {
+        public FirstClass()
+        {
+        }
+        public void FirstClassPrint()
+        {
+            Console.WriteLine("안녕하세요. FirstClass 입니다.");
+        }
+    }
+}
