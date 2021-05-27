a80a2068dc484b69eeccf0c0ca021a86b77c92ca
HongEunbeen <37995685+HongEunbeen@users.noreply.github.com>
2021-05-27 22:14:42 +0900

    feat : 08 . Generic List와 사용자 정의 클래스

--- CSharpStudy/CSharpStudy/Program.cs
+++ CSharpStudy/CSharpStudy/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace CSharpStudy
 {
@@ -6,8 +7,51 @@ namespace CSharpStudy
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("안녕 C#");
-            Console.Write("안녕 하세요");
+            // 1 2 3 4 5 6 7 8 9 10
+            //List<int> list = new List<int>();
+
+            //list.Add(1);
+            //list.Add(2);
+            //list.Add(3);
+            //list.Add(4);
+            //list.Add(5);
+
+            //foreach (var num in list)
+            //{
+            //    Console.WriteLine(num);
+            //}
+
+            //var list = new List<string>();
+            //list.Add(1);
+
+            //var list = new List<string>()
+            //{
+            //    "text1",
+            //    "text2",
+            //    "text3",
+            //    "text4"
+            //};
+
+            //foreach(var item in list)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            var list = new List<User>() {
+                new User(){
+                    No = 1,
+                    Name = "임똘똘"
+                },
+                new User(){
+                    No = 2,
+                    Name = "권똘똘"
+                }
+            };
+
+            foreach(var user in list)
+            {
+                Console.WriteLine("번호 : " + user.No + " 이름 : " + user.Name);
+            }
         }
     }
 }
---
+++ CSharpStudy/CSharpStudy/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace CSharpStudy
+{
+    public class User
+    {
+        public User()
+        {
+
+        }
+        //번호, 이름, 나이, 연락처
+        public int No { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string phone { get; set; }
+    }
+}
