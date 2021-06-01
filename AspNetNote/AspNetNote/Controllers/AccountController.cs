329bcc0bda146ff2a73635693d95c030edd0e073
HongEunbeen <37995685+HongEunbeen@users.noreply.github.com>
2021-06-02 01:14:42 +0900

    feat : 10. 로그인 입력 Form 만들기

--- AspNetNote/AspNetNote/Controllers/AccountController.cs
+++ AspNetNote/AspNetNote/Controllers/AccountController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using AspNetNote.DataContext;
 using AspNetNote.Models;
+using AspNetNote.ViewModel;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -34,6 +35,32 @@ namespace AspNetNote.Controllers
         public IActionResult Login()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(LoginViewModel model)
+        {
+            //ID, 비밀번호 - 필수
+            if (ModelState.IsValid)
+            {
+                using (var db = new AspNetNoteDbContext())
+                {
+                    //Linq query - 메서드 체이닝
+                    var user = db.Users.FirstOrDefault
+                        (u => u.UserId.Equals(model.UserId) && u.UserPassword.Equals(model.UserPassword));
+
+                    if(user != null)
+                    {
+                        //로그인에 성공했을 때
+                        return RedirectToAction("LoginSuccess", "Home");
+                     }
+                    //로그인에 실패했을 떄
+                    //사용자 ID 자체가 회원가입 X 경/
+                    ModelState.AddModelError(string.Empty, "사용자 ID 혹은 비밀번호가올바르지 않습니다.");
+
+                }
+            }
+            return View(model);
         }
 
         [HttpPost]
--- AspNetNote/AspNetNote/Controllers/HomeController.cs
+++ AspNetNote/AspNetNote/Controllers/HomeController.cs
@@ -33,5 +33,10 @@ namespace AspNetNote.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public IActionResult LoginSuccess()
+        {
+            return View();
+        }
     }
 }
