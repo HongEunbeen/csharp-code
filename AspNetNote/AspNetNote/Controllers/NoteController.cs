6e7eb491477b6d0f0643b90e9f009586814b8938
HongEunbeen <37995685+HongEunbeen@users.noreply.github.com>
2021-06-04 23:00:09 +0900

    12. 게시판 리스트 출력하기

---
+++ AspNetNote/AspNetNote/Controllers/NoteController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AspNetNote.DataContext;
+using AspNetNote.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace AspNetNote.Controllers
+{
+    public class NoteController : Controller
+    {
+        /// <summary>
+        /// 게시판 리스트
+        /// </summary>
+        /// <returns></returns>
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            using(var db = new AspNetNoteDbContext())
+            {
+                var list = db.Notes.ToList();
+                return View(list);
+            }
+        }
+
+        /// <summary>
+        /// 게시물 추가
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// 게시물 수정
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Edit()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// 게시물 삭제
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Delet()
+        {
+            return View();
+        }
+    }
+}
